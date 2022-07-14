2d2f8ece1c29616c53c2da8b6548e71e3a10c038
Galina <gpromanova@gmail.com>
2022-07-14 09:23:23 +0300

    #2 Sprint 2 Service user friends and film likes

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
@@ -12,43 +15,65 @@ import java.util.*;
 
 @Validated
 @RestController
+@NoArgsConstructor
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    private final Map<Integer, User> users = new LinkedHashMap<>();
-    private int nextId = 0;
 
-    private int getNextId() {
-        return ++nextId;
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users")
     public Collection<User> findAllUsers() {
-        log.debug("Текущее количество пользователей: {}", users.size());
-        return users.values();
+        log.debug("Текущее количество пользователей: {}", userService.findAllUsers().size());
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getUser(@PathVariable long userId) {
+        log.info("Get user by id={}", userId);
+        return userService.get(userId);
     }
 
     @PostMapping(value = "/users")
     public User createUser(@Valid @RequestBody User user) {
-        user.setId(getNextId());
-        if (user.getName().isBlank() || user.getName() == null) {
-            user.setName(user.getLogin());
-        }
-        users.put(user.getId(), user);
+        userService.saveUser(user);
         log.debug("количество добавленных пользователей: {}", 1);
         return user;
     }
 
     @PutMapping(value = "/users")
     public User update(@Valid @RequestBody User user) {
-        if (!users.containsKey(user.getId())) {
-            RuntimeException e = new ValidationException("Пользователь с таким Id не найден");
-            log.error(e.getMessage());
-            throw e;
-        }
-        users.put(user.getId(), user);
+        userService.updateUser(user);
         log.debug("Данные пользователя обновлены");
         return user;
     }
 
+    @PutMapping("/users/{userId}/friends/{friendId}")
+    public void addFriend(@PathVariable long userId, @PathVariable long friendId) {
+        userService.addFriend(userId, friendId);
+        log.debug("Добавлен еще один друг");
+    }
+
+    @DeleteMapping("/users/{userId}/friends/{friendId}")
+    public void deleteFriend(@PathVariable long userId, @PathVariable long friendId) {
+        userService.removeFriend(userId, friendId);
+        log.debug("Друг с id= {} удален из списка", friendId);
+    }
+
+    @GetMapping("/users/{userId}/friends/common/{otherId}")
+    public Collection<User> findCommonFriends(@PathVariable long userId, @PathVariable long otherId) {
+        log.debug("Количество общих друзей: {}", userService.findAllCommonFriends(userId, otherId).size());
+        return userService.findAllCommonFriends(userId, otherId);
+    }
+
+    @GetMapping("/users/{userId}/friends")
+    public Collection<User> findAllFriends(@PathVariable long userId) {
+        log.debug("Количество друзей: {}", userService.getListOfFriends(userId).size());
+        return userService.getListOfFriends(userId);
+    }
 }
 
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,23 +2,37 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
 import ru.yandex.practicum.filmorate.controllers.After;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Film {
-    private int id;
+    private long id;
     @NotBlank(message = "название не может быть пустым и содержать пробелы")
-    private final String name; // название
+    private String name; // название
     @After("1895-12-28")
-    private final LocalDate releaseDate; // дата создания
+    private LocalDate releaseDate; // дата создания
     @Size(max = 200, message = "максимальная длина описания — 200 символов")
     private String description; // описание
     @Min(value = 0, message = "продолжительность фильма должна быть положительной")
     private long duration; // длительность
+    @JsonIgnore
+    private Set<Long> likes = new HashSet<>();//хранение информации о лайках: «один пользователь — один лайк»
+
+    public Film(String name, LocalDate releaseDate, String description, long duration) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.description = description;
+        this.duration = duration;
+    }
 }
